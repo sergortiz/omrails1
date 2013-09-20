43050129d2cfa0ce3504de1a5a7d165eb3c9acc9
sergio raya <sergnotes@gmail.com>
2013-09-20 00:42:37 -0700

    Add Pin Associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,10 @@ class Pin < ActiveRecord::Base
   attr_accessible :description
 
 
-
   validate :description, presence: true
+
+  belongs_to :user
+  validates :user_id, presence: true
+  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130826023245) do
+ActiveRecord::Schema.define(:version => 20130920004758) do
 
   create_table "installs", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -35,7 +35,10 @@ ActiveRecord::Schema.define(:version => 20130826023245) do
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], :name => "index_pins_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
